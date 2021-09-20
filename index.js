88cf286c5336dafc26e522d83ebda680cbeac932
JCoDog <jcodog@jconet.co.uk>
2021-09-20 03:21:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,6 @@ client.on('message', (channel, tags, message, self) => {
         let now = new Date();
         let joined = new Date('2021-09-18T20:00:00');
         let diffFormatted = formatDistance(now, joined, {includeSeconds: true})
-        client.say(channel, `@${tags.username}, Kiy is officially part of Team Xcluded as a Content Creator! She has been a signed member publically for ${diffFormatted}!!!! Check the other amazing people out here: https://teamxcluded.com/`);
+        client.say(channel, `@${tags.username}, Kiy is officially part of Team Xcluded as a Content Creator! She has been a signed member publicly for ${diffFormatted}!!!! Check the other amazing people out here: https://teamxcluded.com/`);
     }
 });
