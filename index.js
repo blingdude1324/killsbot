9e5eb4607f86320b4679e449ed39cf4486d873a0
JCoDog <jcodog@jconet.co.uk>
2021-09-20 03:22:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ client.on('message', (channel, tags, message, self) => {
 
     if(command === "team xcluded") {
         let now = new Date();
-        let joined = new Date('2021-09-18T20:00:00');
+        let joined = new Date('2021-09-18T20:00:00'); // uses UTC for the time hence +8h on estimated time+date.
         let diffFormatted = formatDistance(now, joined, {includeSeconds: true})
         client.say(channel, `@${tags.username}, Kiy is officially part of Team Xcluded as a Content Creator! She has been a signed member for ${diffFormatted}!!!! Check the other amazing people out here: https://teamxcluded.com/`);
     }
