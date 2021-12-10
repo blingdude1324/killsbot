bd06860fdfa9d2b6b7b0fece57fcb8ddc9c6a29a
JCoDog <jcodog@jconet.co.uk>
2021-12-10 16:46:11 +0000

    v3.0.0

--- index.js
+++ index.js
@@ -20,5 +20,10 @@ client.on('message', (channel, tags, message, self) => {
 
     let command = message.toLowerCase();
 
-	
+	if (command === "!wet") {
+		let min = Math.ceil(0);
+		let max = Math.floor(100);
+		let percentage = Math.floor(Math.random() * (max - min + 1)) + min;
+		client.say(channel, `${tags.username}, you are ${percentage}% wet!`);
+	}
 });
