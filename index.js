0f0a727ae3b2fc6bbc85a1aa989663ece2176640
JCoDog <jcodog@jconet.co.uk>
2021-12-10 19:26:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('message', async (channel, tags, message, self) => {
   	let args = messageArray.slice(1);
 
 	if (message.startsWith(prefix)) {
-		let command = commands.get(commandName);
+		let command = commands.fine(cmd => cmd.name === commandName);
     	if (!command) return;
 	
 		try {
